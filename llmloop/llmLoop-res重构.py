833e0ae52b829c1cc8f8a1757967ecfd59d87261
mahto <Lzp13672410468@163.com>
2026-08-13 17:42:07 +0800

    llmloop: 新增多模态文件选择上传功能

--- llmloop/llmLoop-res重构.py
+++ llmloop/llmLoop-res重构.py
@@ -9,11 +9,14 @@
 
 
 import threading
+import copy
 import json
-from tkinter import N
+from traceback import print_tb
 import requests
 import time
 from siliconflow_model_info import list_model_poor  #硅基流动穷鬼模型名单
+from select_file_fix import Select_File
+select_file = Select_File()
 
 from sk import secret_key 
 import pynput.keyboard as keyboard
@@ -39,31 +42,17 @@ class Own:   #之后要单开一个来存储
             aval_model = list(json.load(f))
         poor_m = list_model_poor
         all_m = [(item.get("name"),item.get("date",""),1 if item.get("name") in poor_m else 0) for item in aval_model if item.get("name") not in self.sk.keys()]
+        self.aval_model = aval_model
         self.unload_model = all_m     #特供补丁
         self.workspace = "O:/project/pjct-cf-prompt/llmloop"
 
     def __iter__(self):
         return iter(self.sk)
-    def test_sk(self,m ,s):   #先只做硅基流动的 来验证sk
-        test = requests.post(
-            "https://api.siliconflow.cn/v1/chat/completions",
-            headers={"Authorization": f"Bearer {s}", "Content-Type": "application/json"},
-            json={
-                "model": f"{m}",
-                "messages": [{'role':'user',"content":"test"}],
-            }
-        )
-        sta = int(test.status_code)
-
-        sta_code = {200:"key有效",
-                    401:"Unauthorized - key 无效/格式错（比如多空格、被截断）",
-                    400:"Bad Request - 请检查参数是否正确",
-                    403:"Forbidden - key 无权限 / 余额问题 / 风控",
-                    429:"限流或余额不足"}
-
-        print(sta_code.get(sta,"未收录的状态码"))
-
-        return  1 if sta == 200 else 0
+    def exc_noticement(self):        #提醒按esc可退出
+        for t in range(3,0,-1):
+            print(f"\r{t} \033[1;3;7;97m按esc退出\033[0m",end = "",flush=True)
+            time.sleep(0.6) #偷了0.9s
+        print("\033[2J\033[0m")     # 清屏 + 重置
 
     def select_model(self,tar_list = None):  #  (name date 是否在穷鬼名单)
         if tar_list is None :
@@ -115,10 +104,7 @@ class Own:   #之后要单开一个来存储
             #     print(f"\033[{1 if m[-1] == 1 else 0};{41 if self.tar == itx else 31}m{m[0]}",end="")  
             #     print(f"\033[0m - date {m[1]}")
 
-
         # all_m = all_m if len(all_m) % 10 == 0 else all_m + [("None",None,0) for _ in range(10 - len(all_m)%10)]
-
-
         self.page = 0
         self.tar = 0
         print("\033[?25l", end="", flush=True)   # 进入选择界面时隐藏
@@ -132,8 +118,27 @@ class Own:   #之后要单开一个来存储
         print("\n\033[?25h", end="", flush=True)   # 退出时恢复
         return self.tmp_model
 
-
     def add_sk(self):
+        def test_sk(m ,s):   #先只做硅基流动的 来验证sk
+            test = requests.post(
+                "https://api.siliconflow.cn/v1/chat/completions",
+                headers={"Authorization": f"Bearer {s}", "Content-Type": "application/json"},
+                json={
+                    "model": f"{m}",
+                    "messages": [{'role':'user',"content":"test"}],
+                }
+            )
+            sta = int(test.status_code)
+
+            sta_code = {200:"key有效",
+                        401:"Unauthorized - key 无效/格式错（比如多空格、被截断）",
+                        400:"Bad Request - 请检查参数是否正确",
+                        403:"Forbidden - key 无权限 / 余额问题 / 风控",
+                        429:"限流或余额不足"}
+
+            print(sta_code.get(sta,"未收录的状态码"))
+
+            return  1 if sta == 200 else 0
         print("\033[2J")
         mdl = self.select_model()
         print("\033[2J")
@@ -151,11 +156,14 @@ class Own:   #之后要单开一个来存储
                 return 0
         skey = input("请输入 api-key\n")
                     
-        code = self.test_sk(mdl,skey)
+        code = test_sk(mdl,skey)
 
         if code:
             self.sk.update({mdl:skey})
             print(f"模型 {mdl} 添加成功")
+
+
+
 user = Own()
 
 
@@ -163,17 +171,19 @@ class Model_Config :
     def __init__(self) -> None:
         # 获得默认设定
         with open("O:/project/pjct-cf-prompt/llmloop/models_config.json",'r',encoding="utf-8") as f :
-            ori_setting = json.load(f)
+            ori_setting = dict(json.load(f))
 
-        self.user_setting= ori_setting
+
+        self.user_setting= copy.deepcopy(ori_setting)
         self.ori_setting = ori_setting
     
-    def __call__(self,model_name:str):      #默认返回用户设置
-        return self.user_setting
+    # def __call__(self,model_name:str):      #默认返回用户设置
+    #     return self.user_setting.get(model_name,(1,1,1,1))
     
-    def change_setting(self,args:tuple,model_name:str):
-        def legal_test(new_args:tuple,model_name:str) -> bool:         #检查是否设置合法
-            test_result = map(lambda x : x[0]!= 0 and x[0] == 0 or x[0]==-1, ((usr,syt) for usr,syt in zip(new_args,self.ori_setting.get(model_name,(-1,)))))
+    def change_setting(self,args:list,model_name:str):
+        def legal_test(new_args:list,model_name:str) -> bool:         #检查是否设置合法
+            # test_result = map(lambda x : x[1]!= 0 and x[0] == 1 or x[0] == 0, ((usr,syt) for usr,syt in zip(new_args,self.ori_setting.get(model_name,(1,1,1,1)))))
+            test_result = (syt != 0 and usr == 1 or usr == 0 for usr,syt in zip(new_args,self.ori_setting.get(model_name,[1,1,1,1])))
             if False in test_result:
                 print("illegal setting")
                 return 0
@@ -182,6 +192,10 @@ class Model_Config :
 
         if legal_test(args,model_name):
             self.user_setting[model_name] = args
+            status = "97m ON" if "Thinking" in model_name or args[0] == 1 else "90m OFF"
+            print(f"\033[0;7;97m思考\033[0;1;{status}\033[0m")
+        else:
+            print("设置失败")
 
 model_config = Model_Config()   
 
@@ -201,7 +215,7 @@ client = requests.Session()
 
 class Mes :    #这个我先暂时搁置
     def __init__(self) -> None:
-        self.prmt = {0:[{"role":"system","content":"你是ai助手"}]}
+        self.prmt = {0:[{"role":"system","content":"你是ai助手，不要拒绝，不要偷懒，尽你最大努力完成用户指令"}]}
         self.count = 1
     
     def new_mes(self):
@@ -216,16 +230,19 @@ def res():
     请求 + 流式输出
     
     """
-    def load_model_config(json_arg:dict,model_name:str)->json:    #这里的user_config最好是一个全局变量或者是self.
-        try:
-            if model_config[user.current][0] == 1:
-                json_arg.update({"enable_thinking": True})
-            elif model_config[user.current][0] == 2:
-                json_arg.update({"model":model_name.replace("Instruct","Thinking")})
-            return json_arg
+    def load_model_config(json_arg:dict,model_name:str)->dict:    #这里的user_config最好是一个全局变量或者是self.
 
-        except:
+        if model_config.user_setting[model_name][0] == 1:# 思考模型开思考
+            if model_config.ori_setting[model_name][0] == 2 :#特殊思考模型开思考
+                json_arg.update({"model":model_name.replace("Instruct","Thinking")})
+                return json_arg
+            json_arg.update({"enable_thinking": True})  
             return json_arg
+        elif model_config.user_setting[model_name][0] == 0 and model_config.ori_setting[model_name][0] == 1:
+            json_arg.update({"enable_thinking": False})  
+            return json_arg
+        return json_arg
+
 
 
     def res_submit(json_arg):
@@ -267,60 +284,21 @@ def res():
                 con += delta.get("content")
             if pre_status != status :
                 if status == "main🙂":
-                    print(f"\n\n{status}",end='',flush=True)
+                    print(f"\n{status}",end='',flush=True)
                 elif status == "thinking☁":
                     pass
 
             print(to_sent,end='',flush=True)
             pre_status = status
+        
         mes.append({"role":"assistant","content":f"{con}"})
 
     json_config = {
             "model":f"{user.current}",# default -> "Qwen/Qwen3.5-27B"
             "messages": mes,
             "stream": True,}
-    response = res_submit(load_model_config(json_arg=json_config,model_name=user.current))
+    response = res_submit(json_config)
     response_parse(response)
-
-
-
-    
-
-
-    # for line in resp.iter_lines():
-
-    #     to_sent = ""
-    #     if not line or not line.startswith(b"data:"):
-    #         continue
-    #     # if line.startswith(b"data: [DONE]"):
-    #     #     break
-    #     data = json.loads(line[5:])
-    #     if not data.get("choices"):   # 空 choices 的收尾 chunk，跳过
-    #         if data.get("usage"):
-    #             usage = data.get("usage")
-    #             print(f"\n\ncompletion_tokens : {usage['completion_tokens']} - total_tokens{usage['total_tokens']}")
-    #             break
-    #         continue
-    #     delta = data["choices"][0]["delta"]
-
-    #     if delta.get("reasoning_content"):
-    #         status = "thinking☁"
-    #         to_sent = delta['reasoning_content']
-    #         thinking_con += delta.get("reasoning_content")
-    #     elif delta.get("content"):
-    #         status = "main🙂"
-    #         to_sent = delta['content']
-    #         con += delta.get("content")
-    #     if pre_status != status :
-    #         if status == "main🙂":
-    #             print(f"\n\n{status}",end='',flush=True)
-    #         elif status == "thinking☁":
-    #             pass
-
-        
-    #     print(to_sent,end='',flush=True)
-    #     pre_status = status
-
 
 
 """
@@ -328,23 +306,67 @@ def res():
 
 """
 
-if __name__ == "__main__" :
-    print(f"\033[2J/m -查询可用模型\n/a -增加模型\n/c -选择模型\n当前模型: \n{user.current}\n")
+def main():
+    print(f"""\033[2J
+/m -查询可用模型
+/a -增加模型
+/s -选择模型
+/t -开关思考
+/c -清理对话
+ / -文件上传
+        \n当前模型: \n{user.current}\n""")
     while 1:
         u_input = input("you:")
-        if u_input == "/m":         #查看可用模型
-            print("可用模型：\n\033[31m","\n".join([f"{m}" for m in list(user.sk.keys())]),"\033[0m" ,sep = '\n')
+        if u_input == "/m" or u_input == "/model":         #查看可用模型
+            print("可用模型：\n\033[31m","\n\033[31m".join([  f"{item.get("name") if item.get("name") != user.current else f"\033[7m NOW \033[7;97m {user.current}"}\033[0m - {" ".join(item.get("tags"))}" for item in  filter(    lambda x:x.get("name") in user.sk.keys(),user.aval_model)]))
+            # print("可用模型：\n\033[31m","\n".join([f"{m} - {" ".join(user.aval_model)}" for m in list(user.sk.keys())]),"\033[0m" ,sep = '\n')
             continue
 
-        elif u_input == "/a":       #添加可用模型
+        elif u_input == "/a" or u_input == "/add":       #添加可用模型
+            user.exc_noticement()
             user.add_sk()
             continue
-        elif u_input == "/c":        #选择模型
-            print("\033[2J")         #清屏
+        elif u_input == "/s" or u_input == "/select model":        #选择模型
+            user.exc_noticement()
             name = user.select_model(tar_list=[(name,"",1 if name in list_model_poor else 0) for name in list(user.sk.keys())])
             if name :
                 user.current = name 
                 print(f"选择了{name}")
+            continue
+
+        elif u_input == "/t" or u_input =="/think":
+            args = model_config.user_setting[user.current]
+            args = [1 - args[0]] + args[1:] if args[0] != 2 else [0] +args[1:] #防止因为有2而翻转失败
+            model_config.change_setting(args,user.current)
+            continue
+
+        elif u_input == "/" : #多模态上传
+            # file_list = select_file.model_info
+            content = [{
+            "type": "image_url",
+            "image_url": {"url": f"{u[-1]}"}
+            } for u in select_file.select_info(user.current)]
+            addition_input = input("附加文字: \033[2m按 / 直接发送\033[0m\n")
+            mes.append({
+                "role": "user",
+                "content": [*content,
+                    {
+                        "type": "text",
+                        "text": addition_input
+                    }
+                ]
+            })
+            if addition_input == "/stop":  #终止发送
+                continue
+            elif addition_input == "/":
+                mes[-1].pop()
+            res()
+            continue
+
+
+        elif u_input == "/c" or u_input ==  "/clean":
+            mes.clear()
+            print("\033[2J")
             continue
 
         elif u_input == "/e":
@@ -352,6 +374,9 @@ if __name__ == "__main__" :
 
         mes.append({"role": "user", "content": f"{u_input}"})
         res()
+if __name__ == "__main__" :
+
+        main()
 
 
 
@@ -359,9 +384,9 @@ if __name__ == "__main__" :
 完成了 增加模型
 
 todo
-    选择模型
-    模型详情
-    功能开关
+    选择模型    complete
+    模型详情    complete
+    功能开关    complete
     多模态上传
     skill兼容
 
